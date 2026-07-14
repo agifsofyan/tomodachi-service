c964a6f9d08be6b338d6e729231ae53c64396c8f
Agif Sofyan <alpenizme@gmail.com>
2026-07-14 15:32:09 +0700

    refactor start bash #3

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-
 set -e
+
+echo "===== START.SH EXECUTED ====="
+
+pwd
+ls -la
+ls -la app
 
 echo "Running migrations..."
 alembic upgrade head
+
+echo "Testing import..."
+python -c "from app.main import app; print(app)"
 
 echo "Starting FastAPI..."
 
