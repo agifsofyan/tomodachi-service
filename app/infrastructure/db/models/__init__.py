8c6aaa974d9123a32a8be74ab19a630919aacc68
Agif Sofyan <alpenizme@gmail.com>
2026-07-27 13:15:02 +0700

    add model init

---
+++ app/infrastructure/db/models/__init__.py
@@ -0,0 +1,13 @@
+from .address_model import AddressModel
+from .interest_model import InterestModel
+from .profile_interest_model import ProfileInterestModel
+from .profile_model import ProfileModel
+from .user_model import UserModel
+
+__all__ = [
+    "AddressModel",
+    "InterestModel",
+    "ProfileInterestModel",
+    "ProfileModel",
+    "UserModel",
+]
